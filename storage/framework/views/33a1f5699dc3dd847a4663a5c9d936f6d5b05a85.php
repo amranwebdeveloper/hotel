12b56a5ca76253217985f9b6c2d82f91c8f8c8e1
Amran <amranwebdeveloper@gmail.com>
2024-04-17 01:17:33 +0600

    git ignore

--- storage/framework/views/33a1f5699dc3dd847a4663a5c9d936f6d5b05a85.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar-widget">
-    <div class="sidebar-title">
-        <h2><?php echo e($item->title); ?></h2>
-    </div>
-    <div class="textwidget">
-        <?php echo e($item->content); ?>
-
-    </div>
-</div><?php /**PATH /home/amraezyf/hotel.amranwebdeveloper.com/modules/News/Views/frontend/layouts/sidebars/content_text.blade.php ENDPATH**/ ?>
--- storage/framework/views/3ff3bd7feff70a1c087baace73918f57c5813177.php
+++
@@ -1,8 +0,0 @@
-<?php if($translation->content): ?>
-    <div class="g-overview">
-        <div class="description">
-            <?php echo $translation->content ?>
-        </div>
-    </div>
-<?php endif; ?>
-<?php /**PATH /home/amraezyf/hotel.amranwebdeveloper.com/modules/Location/Views/frontend/layouts/details/location-overview.blade.php ENDPATH**/ ?>
